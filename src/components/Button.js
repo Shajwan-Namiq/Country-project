620d5ffff84d48db80a7b1c27d488bbf96994168
Shajwan <shajwannamiqsalh@gmail.com>
2023-03-04 15:02:33 +0300

    update

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,8 +10,8 @@ export default function ButtonFilter({ region, handleRegionFilter ,active,setAct
           setActive(region);
           handleRegionFilter(region);
         }}
-        className={`py-3 rounded-md 
-        ${active === region ? "bg-red-800 text-black" : "bg-gray-200 text-black"} 
+        className={`py-3 rounded-md text-black
+        ${active === region ? "bg-red-800 " : "bg-gray-200"} 
         `}
       >
         {region}
